4da53dfa54b07172640df210bdc1a19eb5d20f95
ykseo88 <ykseo88@gmail.com>
2025-09-14 18:14:04 +0900

    로딩 아이콘 애니메이터 사용

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using Unity.VisualScripting;
+using UnityEngine.Events;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("화면상태")] 
+    [SerializeField] private Image startPage;
+    [SerializeField] private Image endPage;
+    
     [Header("버튼")]
     [SerializeField] private Button doorButton;
     [SerializeField] private Button backButton;
@@ -16,14 +22,19 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject door;
     [SerializeField] private float doorSpeed = 0.5f;
     [SerializeField] private float minCloseStayTime = 1f;
+    
+    [Header("참조")]
 
-    private Action backEffect;
+    private Action<bool> _backEffect;
+    private Action Change;
     private Vector2 originPos;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        doorButton.onClick.AddListener(OnDoor);
+        doorButton.onClick.AddListener(() => OnDoorEffect(true));
+        backButton.onClick.AddListener(Back);
         originPos = door.transform.position;
     }
 
@@ -33,16 +44,28 @@ public class UIManager : MonoBehaviour
         
     }
 
-    private void OnDoor()
+    private void Back()
     {
-        backEffect = OnDoor;
-        door.transform.DOLocalMoveY(0, 1f *  doorSpeed);
-        StartCoroutine(OffDoor());
+        _backEffect(false);
     }
 
-    private IEnumerator OffDoor()
+    private async void OnDoorEffect(bool isToEnd)
     {
-        yield return new WaitForSeconds(minCloseStayTime);
+        _backEffect = OnDoorEffect; // 복귀 효과 저장
+        await DownDoor();
+        await ChangePage(isToEnd);//화면 전환 대기
+        await Task.Delay((int)minCloseStayTime*1000);//
         door.transform.DOLocalMoveY(originPos.y, 1f *  doorSpeed);
+    }
+
+    private async Task ChangePage(bool isToEnd)
+    {
+        endPage.gameObject.SetActive(isToEnd);
+        await Task.Yield();
+    }
+
+    private async Task DownDoor()
+    {
+        await door.transform.DOLocalMoveY(0, 1f * doorSpeed).AsyncWaitForCompletion();
     }
 }
